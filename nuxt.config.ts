7591271dae3fc96ca54249f9d30124465244f99c
delano <delano@onetimesecret.com>
2025-01-24 10:22:06 -0800

    [#84] Lint

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,23 +1,20 @@
-
 // The rest of your Nuxt config...
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  extends: ['@nuxt/ui-pro'],
+  extends: ["@nuxt/ui-pro"],
 
-  builder: 'vite',
+  builder: "vite",
 
   modules: [
-    '@nuxt/content',
-    '@nuxt/eslint',
-    '@nuxt/image',
-    '@nuxt/ui',
-    '@vueuse/nuxt',
-    'nuxt-og-image',
-    'nuxt-security',
+    "@nuxt/content",
+    "@nuxt/eslint",
+    "@nuxt/image",
+    "@nuxt/ui",
+    "@vueuse/nuxt",
+    "nuxt-og-image",
+    "nuxt-security",
   ],
-  ui: {
-
-  },
+  ui: {},
   nitro: {
     prerender: {
       concurrency: 250,
@@ -25,21 +22,35 @@ export default defineNuxtConfig({
     },
     publicAssets: [
       {
-        dir: 'public',  // This will include the entire public directory
-        maxAge: 60 * 60 * 24 * 7  // Cache for 1 week (adjust as needed)
-      }
-    ]
+        dir: "public", // This will include the entire public directory
+        maxAge: 60 * 60 * 24 * 7, // Cache for 1 week (adjust as needed)
+      },
+    ],
   },
   content: {
     // or you might have 'mdc' instead of 'content' depending on your setup
     highlight: {
-      theme: 'one-dark-pro',
-      themes: ['houston', 'aurora-x'],
-      langs: ['javascript', 'typescript', 'python', 'perl', 'ruby', 'go', 'powershell', 'csharp', 'bash', 'html', 'css', 'markdown', 'xml']
-    }
+      theme: "one-dark-pro",
+      themes: ["houston", "aurora-x"],
+      langs: [
+        "javascript",
+        "typescript",
+        "python",
+        "perl",
+        "ruby",
+        "go",
+        "powershell",
+        "csharp",
+        "bash",
+        "html",
+        "css",
+        "markdown",
+        "xml",
+      ],
+    },
   },
   image: {
-  //  dir: 'assets/img'
+    //  dir: 'assets/img'
   },
 
   /**
@@ -58,7 +69,7 @@ export default defineNuxtConfig({
    *    $ envsubst < nuxt.config.ts > nuxt.config.ts.tmp && mv nuxt.config.ts.tmp nuxt.config.ts
    */
   uiPro: {
-    license: process.env.NUXT_UI_PRO_LICENSE || '${NUXT_UI_PRO_LICENSE}',
+    license: process.env.NUXT_UI_PRO_LICENSE || "${NUXT_UI_PRO_LICENSE}",
   },
   postcss: {
     plugins: {
@@ -66,56 +77,55 @@ export default defineNuxtConfig({
       autoprefixer: {},
     },
   },
-  css: [
-    '@/assets/css/main.css',
-    '@/assets/css/font.css',
-  ],
+  css: ["@/assets/css/main.css", "@/assets/css/font.css"],
   vite: {
-    assetsInclude: ['@/assets/css/fonts/**/*.woff', '@/**/**/*.woff2'],
+    assetsInclude: ["@/assets/css/fonts/**/*.woff", "@/**/**/*.woff2"],
     server: {
       allowedHosts: true,
     },
   },
   hooks: {
     // Define `@nuxt/ui` components as global to use them in `.md` (feel free to add those you need)
-    'components:extend': (components) => {
-      const globals = components.filter(c => ['UButton'].includes(c.pascalName))
+    "components:extend": (components) => {
+      const globals = components.filter((c) =>
+        ["UButton"].includes(c.pascalName),
+      );
 
-      globals.forEach(c => c.global = true)
-    }
+      globals.forEach((c) => (c.global = true));
+    },
   },
 
   colorMode: {
-    disableTransition: true
+    disableTransition: true,
   },
 
   routeRules: {
     // Temporary workaround for prerender regression. see https://github.com/nuxt/nuxt/issues/27490
-    '/': { prerender: true },
-    '/api/search.json': { prerender: true },
-    '/docs': { redirect: '/docs/introduction', prerender: true }
+    "/": { prerender: true },
+    "/api/search.json": { prerender: true },
+    "/docs": { redirect: "/docs/introduction", prerender: true },
   },
 
   devtools: {
-    enabled: true
+    enabled: true,
   },
 
   typescript: {
-    strict: false
+    strict: false,
   },
 
   future: {
-    compatibilityVersion: 4
+    compatibilityVersion: 4,
   },
 
   eslint: {
     config: {
       stylistic: {
-        commaDangle: 'never',
-        braceStyle: '1tbs'
-      }
-    }
+        commaDangle: "never",
+        braceStyle: "1tbs",
+      },
+    },
   },
 
-  compatibilityDate: '2024-07-11'
-})
+  compatibilityDate: "2024-07-11",
+});
